9bd081a0c6b075f03e2698bd83d458d36a5cdf57
Emperor D. Pengin <emperord.pengin@gmail.com>
2021-07-26 04:51:11 -0400

    small changes for js
    foundation for page .3

---
+++ index.js
@@ -0,0 +1,9 @@
+image.onmouseover = function(){
+    var image = document.getElementById('badge-image');
+    image.src = "Sources/PenginCircle.png";
+}
+
+image.onmouseout = function(){
+    var image = document.getElementById('badge-image');
+    image.src = "Sources/badge.png";
+}
